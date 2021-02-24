e39f93bbd42c887f7aa5e454d9ee2577da6136fc
Alvaro Fernandez <fernandez-alvaro@hotmail.com>
2021-02-24 17:58:11 +0100

    finished

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from time import time
+from prettytable import PrettyTable
+import matplotlib.pyplot as plt
 
 totCount = 0
 
@@ -19,34 +21,105 @@ def calcRecursionDepth(n):
 
 
 def fibEfficient(n):
-    fibArr = [0,1]
+    fibArr = [0, 1]
     if n < len(fibArr):
         return fibArr[n]
 
-    for i in range(n-1):
-        fibArr.append(fibArr[i]+fibArr[i+1])
+    for i in range(n - 1):
+        fibArr.append(fibArr[i] + fibArr[i + 1])
     return fibArr[-1]
 
-FIB_NUMBER = 9
-if FIB_NUMBER > 100000:
-    exit(13)
 
-print("Fib recursive:")
-timestart = time()
-print(str(fib(FIB_NUMBER)))
-print(str(time()-timestart))
-print("Total recursive calls: ", totCount)
+def runRecursiveFibWithDepth():
+    table = PrettyTable(['N', 'Fibonacci', 'Depth'])
+    for N in range(0, 31):
+        tmpArr = []
+        global totCount
+        totCount = 0
+        tmpArr.append(N)
+        tmpArr.append(fib(N))
+        tmpArr.append(totCount)
+        table.add_row(tmpArr)
 
-print("-----")
+    print('Recursive Depth')
+    print(table)
 
-print("Total recursive calls: ")
-print(str(calcRecursionDepth(FIB_NUMBER)))
 
-print("-----")
+def runRecursiveFibAndTime():
+    table = PrettyTable(['N', 'Fibonacci', 'Time'])
+    for N in range(0, 31):
+        tmpArr = []
+        tmpArr.append(N)
+        timestart = time()
+        tmpArr.append(fib(N))
+        timeTotal = time() - timestart
+        tmpArr.append(round(timeTotal*1000, 5))
+        table.add_row(tmpArr)
 
-print("FibEfficient:")
-timestart = time()
-print(str(fibEfficient(FIB_NUMBER)))
-print(str(time()-timestart))
+    print('Recursive Time')
+    print(table)
 
-print("--------------------")
+
+def runIterativeFibAndTime():
+    table = PrettyTable(['N', 'Fibonacci', 'Time'])
+    for N in range(0, 31):
+        tmpArr = []
+        tmpArr.append(N)
+        timestart = time()
+        tmpArr.append(fibEfficient(N))
+        timeTotal = time() - timestart
+        tmpArr.append(round(timeTotal * 1000, 5))
+        table.add_row(tmpArr)
+
+    print('Iterative Time')
+    print(table)
+
+
+def runBothAndPrepareForPlot():
+    nArr = []
+    timeArr = []
+    iterArray = []
+    for N in range(0, 31):
+        nArr.append(N)
+        timestart = time()
+        fibEfficient(N)
+        timeTotal = time() - timestart
+        timeArr.append(round(timeTotal*1000, 5))
+
+    iterArray.append(nArr)
+    iterArray.append(timeArr)
+
+    nArr = []
+    timeArr = []
+    recArray = []
+    for N in range(0, 31):
+        nArr.append(N)
+        timestart = time()
+        fib(N)
+        timeTotal = time() - timestart
+        timeArr.append(round(timeTotal*1000, 5))
+
+    recArray.append(nArr)
+    recArray.append(timeArr)
+
+    return iterArray, recArray
+
+
+def plotBoth():
+    iterArray, recArray = runBothAndPrepareForPlot()
+    plt.plot(iterArray[0], iterArray[1], label="Iterativ")
+
+    plt.plot(recArray[0], recArray[1], label="Rekursiv")
+    plt.xlabel('Nte Zahl')
+    plt.ylabel('Dauer')
+    plt.legend()
+    plt.show()
+
+
+runRecursiveFibWithDepth()
+runRecursiveFibAndTime()
+runIterativeFibAndTime()
+plotBoth()
+
+
+
